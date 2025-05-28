d51163f16d6634dbc61ec42d11a74c1461eb28d7
Andrea Huamani <jandrea121200@gmail.com>
2025-05-28 13:33:42 +0200

    Cambios realizados listos

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-cerrar.addEventListener("click",  () => {
-    nav.classList.remove("visible");
-})
-document.addEventListener("click", (event) => {
-   const esClickDentroDelMenu = nav.contains(event.target);
-   const esClickEnAbrir = abrir.contains(event.target);
- 
-   if (nav.classList.contains("visible") && !esClickDentroDelMenu && !esClickEnAbrir) {
-     nav.classList.remove("visible");
-   }
- });
 
+// Mostrar el menú
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+
+// Ocultar si se hace clic fuera del menú
+document.addEventListener("click", (event) => {
+  const esClickDentroDelMenu = nav.contains(event.target);
+  const esClickEnAbrir = abrir.contains(event.target);
+
+  if (nav.classList.contains("visible") && !esClickDentroDelMenu && !esClickEnAbrir) {
+    nav.classList.remove("visible");
+  }
+});
